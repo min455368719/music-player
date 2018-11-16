6e730491ff7b72381ccaf1af21491b2ae1e26c77
min455368719 <455368719@qq.com>
2018-11-16 11:40:04 +0800

    1.3

---
+++ client/music-player/src/components/Comment.vue
@@ -0,0 +1,9 @@
+<template>
+	<h1>这里是评论部分</h1>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
--- client/music-player/src/components/Player.vue
+++ client/music-player/src/components/Player.vue
@@ -1,11 +1,14 @@
 <template>
-	<div>
+	<div class="biger">
+		<div class="baffle">
+			<img class="blur" src="http://p1.music.126.net/96L4ojVi9N3tdHk3r3GXjQ==/109951163066640079.jpg?param=300x300"/>
+		</div>
 		<el-container class="content">
 			<div class="left">
-				<div>
-					<img src="../../build/logo.png" />
+				<div class="logo">
+					<img src="http://p1.music.126.net/96L4ojVi9N3tdHk3r3GXjQ==/109951163066640079.jpg?param=300x300" />
 				</div>
-				<div>
+				<div class="col">
 					<img src="../../static/img/icon1/backward.png"  />
 					<img src="../../static/img/icon1/zanting.png"  />
 					<img src="../../static/img/icon1/forward.png" />
@@ -13,18 +16,50 @@
 				<div class="block">
     				<el-slider v-model="value1" :show-tooltip="false"></el-slider>
 				</div>
+				<div class="showTime nowTime">00:00</div>
+				<div class="showTime endTime">05:21</div>
 			</div>
-			<div class="right"></div>
+			<div class="right">
+				<div class="r-top">
+					<span class="p1">{{ songMsg[0] }}</span>
+					<span class="p2">陈奕迅</span>
+				</div>
+				<ul class="r-mid">
+					<li v-for="item in songMsg[1]" :key='item'>
+						{{ item }}
+					</li>
+				</ul>
+				<div class="r-bom">
+					<ul>
+						<li>
+							<a href="#"><img src="../../static/img/icon1/xihuan.png" /></a>
+						</li>
+						<li>
+							<a href="#"><img src="../../static/img/icon1/shoucang.png" /></a>
+						</li> 
+						<li>
+							<a href="#"><img src="../../static/img/icon1/xiaoxi.png" /></a>
+						</li>
+						<li>
+							<a href="#"><img src="../../static/img/icon1/fenxiang.png" /></a>
+						</li>
+					</ul>
+				</div>
+			</div>
 		</el-container>
 	</div>
 </template>
 
 <script>
+	var sName='不要说话';
+	var sImg='http://p1.music.126.net/96L4ojVi9N3tdHk3r3GXjQ==/109951163066640079.jpg?param=300x300';
+	var lyrics=['深色的海面布满白色的月光','我出神望着海 心不知飞哪去','听到她在告诉你','说她真的喜欢你','我不知该躲哪里','爱一个人是不是该有默契','我以为你懂得每当我看着你','我藏起来的秘密','在每一天清晨里'];
 	export default {
 		name:'Player',
 	    data() {
 	    	return {
-		        value1: 0
+		        value1: 20,
+		        songMsg:[sName,lyrics,sImg]
 	    	}
 	    },
 	    methods: {
@@ -36,9 +71,110 @@
 </script>
 
 <style scoped>
+	.biger{
+		width: 100%;
+		/*background-color: rgba(245,222,179,0.6);*/
+	}
 	.content{
 		width: 1000px;
-		background-color: yellow;
-		margin: 0 auto;
+		height: 500px;
+		background-color: rgba(76,76,76,0.7);
+		background-size:100%;
+		margin: 20px auto;
+		border-radius: 10px;
+	}
+	.baffle{
+		width: 1000px;
+		height: 500px;
+		position: absolute;
+		left: 175px;
+		z-index: -1;
+	}
+	.blur {	
+	    filter: blur(8px);
+	    width: 100%;
+	    height: 100%;
+	}
+	.left {
+		width: 400px;
+		margin-left: 100px;
+		/*background-color: greenyellow;*/
+		text-align: center;
+	}
+	.logo {
+		width: 240px;
+		height: 240px;
+		margin: 50px auto;
+		border: 10px solid white;
+		border-radius: 50%;
+		overflow: hidden;
+	}
+	.logo img {
+		width: 240px;
+		height: 240px;
+		/*border-radius: 50%;*/
+	}
+	.col img{
+		width: 45px;
+		height: 45px;
+		margin: 0 8px;
+	}
+	.block {
+		width: 240px;
+		margin-left: 80px;
+		margin-top: 10px;
+	}
+	.showTime {
+		position: absolute;
+		font-size: 14px;
+		color: white;
+		top: 600px;
+	}.nowTime {
+		left: 305px;
+	}
+	.endTime {
+		left: 610px;
+	}
+	.right {
+		width: 300px;
+		height: 100%;
+		/*background-color: cyan;*/
+	}
+	.r-top{
+		text-align: center;
+		margin-top: 20px;
+		border-bottom: 1px solid gray;
+	}
+	.r-mid{
+		height: 350px;
+		/*background-color: antiquewhite;*/
+		overflow: hidden;
+	}
+	.p1{
+		font-size: 20px;
+		color: white;
+	}
+	.p2{
+		color: white;
+	}
+	p{
+		margin-left: 30px;
+	}
+	.r-mid li{
+		list-style: none;
+		text-align: center;
+		margin-top: 20px;
+	}
+	.r-bom {
+		height: 50px;
+		margin-top: 10px;
+		/*background-color: olive;*/
+	}
+	.r-bom ul li {
+		width: 24%;
+		height: 100%;
+		/*background-color: yellow;*/
+		text-align: center;
+		display: inline-block;
 	}
 </style>
